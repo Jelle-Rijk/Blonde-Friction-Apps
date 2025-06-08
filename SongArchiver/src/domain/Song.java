98922974185fde0025184780e6e795baab916aa7
Jelle Rijk <jellerijk@gmail.com>
2025-06-08 10:26:37 +0200

    Added equals(), hashCode() and compareTo() + constructors

--- SongArchiver/src/domain/Song.java
+++ SongArchiver/src/domain/Song.java
@@ -1,8 +1,12 @@
 package domain;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+
 import enums.SongStatus;
 
-public class Song {
+public class Song implements Comparable<Song> {
 	private String title;
 
 	private SongStatus status;
@@ -11,15 +15,47 @@ public class Song {
 	/*
 	 * CONSTRUCTORS
 	 */
-	public Song(String title) {
+	public Song(String title, SongStatus status) {
 		setTitle(title);
+		setStatus(status);
+		recordings = new HashSet<Recording>();
 	}
-	
+
+	public Song(String title) {
+		this(title, SongStatus.WORK_IN_PROGRESS);
+	}
+
 	/*
 	 * RECORDINGS
 	 */
-	public addRecording(Recording recording) {
+	public void addRecording(Recording recording) {
 		recordings.add(recording);
+	}
+
+	/*
+	 * COMPARING
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (o == null)
+			return false;
+		if (o == this)
+			return true;
+		if (getClass() != o.getClass())
+			return false;
+		Song s = (Song) o;
+		return title.equals(s.title) && status == s.status;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(title, status);
+	}
+
+	@Override
+	public int compareTo(Song s) {
+		int result = title.compareTo(s.title);
+		return result == 0 ? status.compareTo(s.status) : result;
 	}
 
 	/*
@@ -30,7 +66,8 @@ public class Song {
 	}
 
 	public void setTitle(String title) {
-		// TODO implement input checking
+		if (title == null || title.isBlank())
+			throw new IllegalArgumentException("Please provide a song name.");
 		this.title = title;
 	}
 
@@ -40,6 +77,10 @@ public class Song {
 
 	public void setStatus(SongStatus status) {
 		this.status = status;
+	}
+
+	public Collection<Recording> getRecordings() {
+		return recordings;
 	}
 
 }
